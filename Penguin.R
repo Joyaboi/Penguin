f88c486f6e56ed21ffbf306a467ccac84aa21761
Hexemer <kirstenahexemer@gmail.com>
2025-09-24 14:14:47 +0100

    First commit

---
+++ Penguin.R
@@ -0,0 +1,6 @@
+penguin <- read.table("Penguin Project/penguin_data.txt", header = TRUE)
+View(penguin)
+
+install.packages("tidyverse")
+library(tidyverse)
+BodyByFlipper <- lm(body_mass_g ~ flipper_length_mm)
